314dbfba1f3ca8024514f85fd05a4b96b6511e68
ceona123 <mookmix2554@gmail.com>
2016-04-29 22:34:15 +0700

    #4 ใส่ศัตรูให้กับเกม
    ใส่ศัตรู เมื่อตัวยานพุ่งชนจะทำการจบเกม

--- GameEngine.java
+++ GameEngine.java
@@ -4,6 +4,8 @@
   import java.awt.event.KeyEvent;
   import java.awt.event.KeyListener;
   import java.awt.geom.Rectangle2D;
+  import java.util.ArrayList;
+  import java.util.Iterator;
 
   import javax.swing.Timer;
   
@@ -11,6 +13,7 @@
   	GamePanel gp;
   	private SpaceShip v;
   	private Timer timer;
+    private ArrayList<Enemy> enemies = new ArrayList<Enemy>();
  	public GameEngine(GamePanel gp, SpaceShip v) {
  		this.gp = gp;
  		this.v = v;
@@ -30,8 +33,38 @@
  	public void start(){
  		timer.start();
  	}
-  	private void process(){
+  private void generateEnemy(){
+     Enemy e = new Enemy((int)(Math.random()*390), 30);
+     gp.sprites.add(e);
+     enemies.add(e);
+  }
+ 
+  private void process(){
+      generateEnemy();
+     Iterator<Enemy> e_iter = enemies.iterator();
+     while(e_iter.hasNext()){
+       Enemy e = e_iter.next();
+       e.proceed();
+      
+       if(!e.isAlive()){
+         gp.sprites.remove(e);
+       }
+     }
   		gp.updateGameUI();
+      Rectangle2D.Double vr = v.getRectangle();
+     Rectangle2D.Double er;
+     for(Enemy e : enemies){
+       er = e.getRectangle();
+       if(er.intersects(vr)){
+         die();
+         return;
+       }
+     }
+   }
+ 
+   public void die(){
+     timer.stop();
+    
   	}
  
  	void controlVehicle(KeyEvent e) {
